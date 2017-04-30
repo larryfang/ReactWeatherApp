38f9dee9a8f2d6dfdaa5a4b133fa6b8ae1aaf3d3
larry fang <larry.l.fang@gmail.com>
2017-04-30 14:43:12 +1000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    entry: './app/app.jsx',
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        root: __dirname,
+        alias: {
+            Main: 'app/components/main.jsx',
+            Nav: 'app/components/nav.jsx',
+            Weather: 'app/components/weather.jsx',
+            About: 'app/components/about.jsx',
+            Example: 'app/components/examples.jsx',
+            WeatherForm: 'app/components/WeatherForm.jsx',
+            WeatherMessage: 'app/components/WeatherMessage.jsx',
+            openWeatherMap: 'app/API/openWeatherMap.jsx'
+        },
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+    }
+};
